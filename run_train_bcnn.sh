f2eb0696bba82ae0d79c61ddb6f7a3f16ded5069
smooth-learning <58157423+smooth-learning@users.noreply.github.com>
2019-12-13 09:10:15 +0800

    Add files via upload

--- run_train_bcnn.sh
+++ run_train_bcnn.sh
@@ -17,8 +17,9 @@ export droprate=0.25
 export denoise=True
 export smooth=True
 export label_weight=0.6
+export tk=2
 
-python train.py --net ${NET} --n_classes ${N_CLASSES} --denoise ${denoise} --droprate ${droprate} --smooth ${smooth} --label_weight ${label_weight}  --path ${path} --data_base ${data}  --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --step ${step}
+python train.py --net ${NET} --n_classes ${N_CLASSES} --denoise ${denoise} --droprate ${droprate} --smooth ${smooth} --label_weight ${label_weight}  --path ${path} --data_base ${data}  --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --tk ${tk} --step ${step}
 
 sleep 300
 
@@ -27,5 +28,5 @@ export w_decay=1e-5
 export batchsize=32
 export step=2
 
-python train.py --net ${NET} --n_classes ${N_CLASSES} --denoise ${denoise} --droprate ${droprate} --smooth ${smooth} --label_weight ${label_weight}  --path ${path} --data_base ${data}  --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --step ${step}
+python train.py --net ${NET} --n_classes ${N_CLASSES} --denoise ${denoise} --droprate ${droprate} --smooth ${smooth} --label_weight ${label_weight}  --path ${path} --data_base ${data}  --lr ${lr} --w_decay ${w_decay} --batch_size ${batchsize} --epochs ${epochs} --tk ${tk}  --step ${step}
 
